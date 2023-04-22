5ddd81ab851710ecff3ea41ecd169b154443f24c
pohsunwang <96949018+boshwang3699@users.noreply.github.com>
2023-04-22 22:33:53 +0800

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,5 +1,5 @@
-bison -d -o y.tab.c ex1.y
-gcc -c -g -I.. y.tab.c
-flex -o lex.yy.c ex1.l
+bison -d -o MiniLisp.tab.c MiniLisp.y
+gcc -c -g -I.. MiniLisp.tab.c
+flex -o lex.yy.c MiniLisp.l
 gcc -c -g -I.. lex.yy.c
-gcc -o ex1 y.tab.o lex.yy.o -ll
+gcc -o MiniLisp y.tab.o lex.yy.o -ll
